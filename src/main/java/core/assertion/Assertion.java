f204e4b8c19d622664155aa742dc08f5ab83678d
Anna Sakiv <anna_sakiv@epam.com>
2023-09-08 15:31:09 +0300

    task 3

--- src/main/java/core/assertion/Assertion.java
+++ src/main/java/core/assertion/Assertion.java
@@ -3,7 +3,11 @@ package core.assertion;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class Assertion {
     private static WebDriver driver;
@@ -19,7 +23,8 @@ public class Assertion {
     }
 
     public static void assertSuccessMessageByLocator( By successMessageLocator, String expectedMessage) {
-        WebElement successMessage = driver.findElement(successMessageLocator);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(successMessageLocator));
         String actualMessage = successMessage.getText();
         System.out.println(actualMessage);
         Assert.assertEquals(actualMessage, expectedMessage);
