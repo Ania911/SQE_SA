0d1a495cd905b943ee6719a5ce364cc21daf5cf5
Anna Sakiv <anna_sakiv@epam.com>
2023-09-20 15:35:34 +0300

    All done

--- src/main/java/core/assertion/ApiAssertions.java
+++ src/main/java/core/assertion/ApiAssertions.java
@@ -7,18 +7,21 @@ import org.json.JSONObject;
 public class ApiAssertions {
 
     public static void assertResponseCode(int actualStatusCode, int expectedStatusCode) {
-        Assert.assertEquals(actualStatusCode, expectedStatusCode,
-                "Expected status code: " + expectedStatusCode + ", but got: " + actualStatusCode);
+        String errorMessage = String.format("Expected status code: %d, but got: %d", expectedStatusCode, actualStatusCode);
+        Assert.assertEquals(actualStatusCode, expectedStatusCode, errorMessage);
     }
+
 
     public static void assertJsonField(String responseBody, String fieldName, String expectedValue) {
         JSONObject responseBodyJson = new JSONObject(responseBody);
 
         if (responseBodyJson.has(fieldName)) {
             String actualValue = responseBodyJson.getString(fieldName);
-            Assert.assertEquals(actualValue, expectedValue, "Value of field '" + fieldName + "' in response body does not match the expected value: " + expectedValue);
+            String errorMessage = String.format("Value of field '%s' in response body does not match the expected value: '%s'", fieldName, expectedValue);
+            Assert.assertEquals(actualValue, expectedValue, errorMessage);
         } else {
-            Assert.fail("Field '" + fieldName + "' not found in response body");
+            String errorMessage = String.format("Field '%s' not found in response body", fieldName);
+            Assert.fail(errorMessage);
         }
     }
 }
